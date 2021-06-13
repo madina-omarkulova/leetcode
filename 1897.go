105fc5581ad31645703115edba0268a0094a4152
madina-omarkulova <74660149+madina-omarkulova@users.noreply.github.com>
2021-06-13 10:34:26 +0600

    1897. Redistribute Characters to Make All Strings Equal

---
+++ 1897.go
@@ -0,0 +1,14 @@
+func makeEqual(words []string) bool {
+	count := [26]int{}
+	for _, k := range words {
+		for _, l := range k {
+			count[l-'a']++
+		}
+	}
+	for i := 0; i < 26; i++ {
+		if count[i]%len(words) != 0 {
+			return false
+		}
+	}
+	return true
+}
