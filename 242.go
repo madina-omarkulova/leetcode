561890950ad0835054ace8801eb3b0176298c54d
madina-omarkulova <74660149+madina-omarkulova@users.noreply.github.com>
2022-05-25 01:43:32 +0600

    242. Valid Anagram

--- 242.go
+++ 242.go
@@ -1,22 +1,16 @@
 package main
 
-import (
-	"sort"
-	"strings"
-)
-
 func isAnagram(s string, t string) bool {
 	if len(s) != len(t) {
 		return false
 	}
-	s2 := strings.Split(s, "")
-	t2 := strings.Split(t, "")
-	sort.Strings(s2)
-	sort.Strings(t2)
-	for i, _ := range s2 {
-		if s2[i] != t2[i] {
-			return false
-		}
+	m := [26]int{}
+	for _, r := range s {
+		m[r-'a']++
 	}
-	return true
+	n := [26]int{}
+	for _, r := range t {
+		n[r-'a']++
+	}
+	return m == n
 }
